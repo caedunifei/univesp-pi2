c6c373a90943150481cb57b2b35f1fbd49cf224e
Márcio Portela <marcio.portela@fatec.sp.gov.br>
2025-09-01 22:55:28 -0300

    initial commit

---
+++ univesp_pi2/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using univesp_pi2.Models;
+
+namespace univesp_pi2.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login(LoginViewModel login)
+        {
+            if(login.User == null && login.Password == null)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ univesp_pi2/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace univesp_pi2.Models
+{
+    public class LoginViewModel
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
